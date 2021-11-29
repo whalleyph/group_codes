d3ccc34697a6a448f6e37030def1e993b19e0c21
Bora Karasulu <mssmkc@zx81.csc.warwick.ac.uk>
2021-11-29 16:50:20 +0000

    Repo has been initialised

--- README.md
+++
@@ -1,2 +0,0 @@
-# group_codes
-Shared scripts, tools and codes
--- neb-clean-ase.py
+++
@@ -1,9 +0,0 @@
-echo -n "This will clean the current ASE-NEB calculation. Are you sure? (y: yes): "
-read ans
-
-if [ "$ans" != "y" ]; then
-echo "Nothing is done..."
-exit 1
-fi
-
-rm -rf IMG* image-* idpp.* neb.* slurm-* machine.file.* diffusion-barrier.png
